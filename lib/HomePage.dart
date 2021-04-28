12f8b9ff72351a8c9266a6e984a6069dfc2041e3
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2021-04-28 10:20:00 +0530

    First Page of Sign Up Added

---
+++ lib/HomePage.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: homePage(),
+    );
+  }
+}
+
+class homePage extends StatefulWidget {
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'HomePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,105 +11,159 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //To hide the debug banner on homepage
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+      routes: {
+        '/homePage' : (context)=>HomePage(),
+      },
     );
   }
 }
 
+//Main Homepage widget added
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.white,
+      body: Row(
+        children: [
+          //With this container added the side image big one
+          Container(
+            width: MediaQuery.of(context).size.width*0.3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('asset/images/sideImg.png'),
+                fit: BoxFit.cover
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          //With this container we add the side content of the image
+          Container(
+            width: MediaQuery.of(context).size.width*0.7,
+            padding: EdgeInsets.symmetric(vertical: 60, horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //This row will contain the content of top line which have time and image of cloud for temperatur
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //This text to show the time on screen
+                    Text("06:22 AM",style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: 'avenir',
+                      fontWeight: FontWeight.w500
+                    ),),
+                    Expanded(
+                        child: Container(),
+                    ),
+                    //This container to contain the corner image of cloud
+                    Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('asset/images/cloud.png'),
+                          fit : BoxFit.contain
+                        )
+                      ),
+                    ),
+                    SizedBox(width: 5,),
+                    //This text is to display the current temperature
+                    Text("34 * C",style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'avenir',
+                      fontWeight: FontWeight.w800
+                    ),)
+                  ],
+                ),
+                //This text for display of current date and day
+                Text("Aug 1, 2020 | Wednesday", style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey
+                ),),
+                Expanded(child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //This container for the logo and for the content of rest of screen
+                      Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("asset/images/logo.png"),
+                            fit: BoxFit.contain
+                          )
+                        ),
+                      ),
+                      //This text to display the ewall logo
+                      Text("eWall", style: TextStyle(
+                        fontSize: 50,
+                        fontFamily: "ubuntu",
+                        fontWeight: FontWeight.w600
+                      ),),
+                      SizedBox(height: 10,),
+                      //This text to display the lucrative content
+                      Text("Open An Account For \nDigital E-Wallet Solutions. \nInstant Payouts. \n\nJoin For Free", style: TextStyle(
+                        color: Colors.grey
+                      ),)
+                    ],
+                  ),
+                ),),
+                //This InkWell to provide user an tapping button to go to next page and sign up
+                InkWell(
+                  //When tap it will invoke openHomePage Method
+                  onTap: openHomePage,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Color(0xffffac30),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Center(
+                      //This Row to write text inside our sign up button
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Sign Up", style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700
+                          ),),
+                          Icon(
+                            Icons.arrow_forward,
+                            size: 17,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: Text("Create an account",style: TextStyle(
+                    fontSize: 16,
+                  ),)
+                )
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+  //This Method to navigate to next page when click on sign up button
+  void openHomePage(){
+    Navigator.pushNamed(context, '/homePage');
   }
 }
